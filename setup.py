4ec133aa78303a51ce84a68ac2365c42523c7dda
Matt Ackerman <matt@Matts-MacBook-Pro.local>
2019-12-12 19:54:42 -0600

    added necessary setup stuff.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Matt Ackerman',                   # Type in your name
   author_email = 'ackerman.j.matt@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Matt-Ackerman',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Matt-Ackerman/pull_automator/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['git', 'pull', 'repo'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
     'validators',
